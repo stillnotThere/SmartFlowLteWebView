eaa0962774f342c8fa874e32d0a27b9d1aa0357f
rohan <rohan.mahendroo@itechenvironmental.ca>
2019-04-01 10:39:32 -0400

    Resolved WebView errors

--- app/src/main/java/com/smartflowlite_webview/MainActivity.kt
+++ app/src/main/java/com/smartflowlite_webview/MainActivity.kt
@@ -1,7 +1,6 @@
 package com.smartflowlite_webview
 
 import android.Manifest
-import android.content.Context
 import android.content.pm.PackageManager
 import android.os.Bundle
 import android.util.Log
